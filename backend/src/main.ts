fa2be962fe8ce97fed4880f92353f809046b3da5
redzimskidev <redzimski@hotmail.com>
2022-10-28 22:02:44 +0200

    Fixed swgger specification

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Specshare')
+    .setDescription('Share your machine specs')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
