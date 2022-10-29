bf62e7d329238b8861ea66a5542464f8377fb9ad
redzimskidev <redzimski@hotmail.com>
2022-10-29 10:54:15 +0200

    Added CQRS module

--- backend/src/machine-specification/machine-specification.module.ts
+++ backend/src/machine-specification/machine-specification.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { MachineSpecificationController } from './interface/machine-specification/machine-specification.controller';
 
 @Module({
+  imports: [CqrsModule],
   controllers: [MachineSpecificationController],
+  providers: [],
 })
 export class MachineSpecificationModule {}
