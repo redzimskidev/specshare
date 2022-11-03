4931ac38eda4e681c627884560c1c65bcf048d3f
redzimskidev <redzimski@hotmail.com>
2022-11-03 15:41:20 +0100

    Clean up default controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MachineSpecificationModule } from './machine-specification/machine-specification.module';
 
 @Module({
@@ -11,7 +9,5 @@ import { MachineSpecificationModule } from './machine-specification/machine-spec
     MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
     MachineSpecificationModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
